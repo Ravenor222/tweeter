003e9bd917aba71ea157143a06da38fdb7c3eadb
Nick <ndanvers222@gmail.com>
2020-01-28 23:35:32 +0000

    adding socials to corner of article, code reviewed by mentor

--- public/scripts/tweetHover.js
+++ public/scripts/tweetHover.js
@@ -1,7 +1,9 @@
-$(() => {
-    $(".tweet-container").hover(function() {
-        $(this).css("box-shadow", "10px 10px 10px lightgrey");
-    },function(){
-        $(this).css("box-shadow", "0px 0px");
-    })
-});
+// $(() => {
+//     $(".tweet-container").hover(function() {
+//         $(".username").removeClass("hidden");
+//         $(this).css("box-shadow", "10px 10px 10px lightgrey");
+//     },function(){
+//         $(".username").addClass("hidden");
+//         $(this).css("box-shadow", "0px 0px");
+//     })
+// });
